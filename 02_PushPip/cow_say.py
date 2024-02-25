530f74d9072538048309e7bdc9de908aa4b9d0f7
Franky <nikto8748@gmail.com>
2024-02-25 22:59:24 +0300

    Add possibility to create your own eyes.

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -1,4 +1,4 @@
-from cowsay import cowsay
+from cowsay import cowsay, Option
 from argparse import ArgumentParser
 
 argparser = ArgumentParser(
@@ -9,10 +9,18 @@ argparser = ArgumentParser(
                     """
                     )
 
-argparser.add_argument('message', nargs = '?',
-                    action = 'store',
-                    help = "Cow's message."
+argparser.add_argument('message', 
+                    nargs='?',
+                    default="Hello there!",
+                    action='store',
+                    help="Cow's message."
                     )
 
+argparser.add_argument('-e', '--eyes', 
+                       default=Option.eyes,
+                       action = 'store',
+                       help="Eye-string."
+                       )
+
 args = argparser.parse_args()
-print(cowsay(args.message))
+print(cowsay(args.message, eyes=args.eyes))
