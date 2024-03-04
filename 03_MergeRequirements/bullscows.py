b8988119dbb6afc3bff953d2b6d7d0c33cc00e6c
Franky <nikto8748@gmail.com>
2024-03-04 18:17:47 +0300

    gameplay function added to bullscows module.

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -1,3 +1,5 @@
+from random import choice
+
 def bullscows(guess: str, secret: str) -> (int, int):
     bulls, cows = 0, 0
     were_arr = []
@@ -9,3 +11,22 @@ def bullscows(guess: str, secret: str) -> (int, int):
         were_arr.append(guess[i])
     return (bulls, cows)
 
+def gameplay(ask: callable, inform: callable, words:list[str]) -> int:
+    secret = random.choice(words)
+    attempts_at_all = 0
+    attempt = ""
+    while attempt != secret:
+        attempt = ask("Введите слово: ", words)
+        attempts_at_all += 1
+        inform("Быки: {}, Коровы: {}", b, c)
+    return attempts_at_all
+
+def ask(prompt: str, valid: list[str]=None) -> str:
+    attempt = input(prompt)
+    if valid is not None:
+        while attempt not in valid:
+            attempt = input(prompt)
+    return attempt
+
+def inform(format_string: str, bulls: int, cows: int) -> None:
+    print(format_string.format(bulls, cows))
