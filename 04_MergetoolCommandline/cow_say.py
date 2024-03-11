8fa177ab61bc4eaa1b11243d544dc07332588384
Franky <nikto8748@gmail.com>
2024-03-11 18:29:49 +0300

    Added list_cows command.

--- 04_MergetoolCommandline/cow_say.py
+++ 04_MergetoolCommandline/cow_say.py
@@ -1,5 +1,5 @@
 import cmd, shlex
-from cowsay import cowsay
+from cowsay import list_cows
 
 class Cowsay(cmd.Cmd):
     """
@@ -7,8 +7,18 @@ class Cowsay(cmd.Cmd):
     in the command line.
     """
     prompt = "=)"
+    
+
+    def do_list_cows(self, args):
+        """
+        Show list of available cows.
+        """
+        print(list_cows())
 
     def do_EOF(self, args):
+        """
+        End the commands with EOF.
+        """
         return True
 
 if __name__ == "__main__":
