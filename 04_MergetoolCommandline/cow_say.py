e22556c9deffd7e7434a89836de98849bb2ed887
Franky <nikto8748@gmail.com>
2024-03-11 18:42:54 +0300

    Added make_bubble command.

--- 04_MergetoolCommandline/cow_say.py
+++ 04_MergetoolCommandline/cow_say.py
@@ -1,5 +1,5 @@
 import cmd, shlex
-from cowsay import list_cows
+from cowsay import list_cows, make_bubble
 
 class Cowsay(cmd.Cmd):
     """
@@ -12,12 +12,23 @@ class Cowsay(cmd.Cmd):
     def do_list_cows(self, args):
         """
         Show list of available cows.
+        Usage: list_cows
         """
         print(list_cows())
+
+    def do_make_bubble(self, args):
+        """
+        Print a bubble with text.
+        Usage: make_bubble text TEXT
+        """
+        args = shlex.split(args)
+        print(dict(zip(args[::2], args[1::2])))
+        print(make_bubble(**dict(zip(args[::2], args[1::2]))))
 
     def do_EOF(self, args):
         """
         End the commands with EOF.
+        Usage: EOF
         """
         return True
 
