23d9bfdb5310cd6501703b0f8cf69a026f3c2e08
Franky <nikto8748@gmail.com>
2024-03-18 20:24:57 +0300

    Added who command.

--- 05_DiffPatchNet/serv.py
+++ 05_DiffPatchNet/serv.py
@@ -6,16 +6,16 @@ users = {}
 
 async def chat(reader, writer):
     me = "{}:{}".format(*writer.get_extra_info('peername'))
+    print(me)
 
     my_id = None
     my_queue  = asyncio.Queue()
 
     send = asyncio.create_task(reader.readline())
-    receive = asyncio.create_task(clients[me].get())
+    receive = asyncio.create_task(my_queue.get())
 
     while not reader.at_eof():
         done, pending = await asyncio.wait([send, receive], return_when=asyncio.FIRST_COMPLETED)
-        
         for q in done:
             if q is send:
                 send = asyncio.create_task(reader.readline())
@@ -23,14 +23,23 @@ async def chat(reader, writer):
                 match commands[0]:
                     case "login":
                         if commands[1] not in list_cows():
-                            writer.write("Error: use 'cows' to see existing login name.")
+                            writer.write("Error: use 'cows' to see existing login name.\n".encode())
+                            continue
                         if commands[1] in users.keys():
-                            writer.write("Error: this login exists, use another login name.")
+                            writer.write("Error: this login is already in use, choose another one.\n".encode())
+                            continue
                         my_id = commands[1]
                         users[my_id] = my_queue
+                        print(f"{me} my id is {my_id}")
+                        writer.write(f"Your id now is {my_id}.\n".encode())
+                    case "who":
+                        writer.write(("Online users are:\n" + " ".join(users.keys()) + "\n").encode())
+                    case _:
+                        continue
+                                
 
             elif q is receive:
-                receive = asyncio.create_task(users[my_id].get())
+                receive = asyncio.create_task(my_queue.get())
                 writer.write(f"{q.result()}\n".encode())
                 await writer.drain()
     send.cancel()
